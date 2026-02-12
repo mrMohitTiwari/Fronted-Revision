ba4ca0a7a1b444920ee1e5a9dc2c0a03b894d3c2
Mohit Tiwari <mt0988145@gmail.com>
2026-02-12 15:14:28 +0530

    added databse

---
+++ To do App/Backend/db.js
@@ -0,0 +1,19 @@
+/*
+Tod{
+title:sting,
+description:string,
+completed:boolean
+
+}*/
+const mongoose = require("mongoose")
+const { string } = require("zod")
+const { toDotPath } = require("zod/v4/core")
+mongoose.connect("mongodb+srv://mt0988145_db_user:chalbhag@cluster0.f8znkl1.mongodb.net/todos").then(()=>console.log("mongoose connected successfully ")).catch(()=>console.log("Error while connecting to database"))
+const todoSchema = mongoose.Schema({
+    title:String,
+    description:String,
+    completed:Boolean
+})
+const todo = mongoose.model('todos',todoSchema)
+module.exports({todo})
+// mongoose.connect('mongodb+srv://mt0988145_db_user:chalbhag@cluster0.f8znkl1.mongodb.net/userappnew').then(() => console.log("mongoose connected Suceefully")).catch(() => console.log("Error while connecting to Server"))
--- To do App/Backend/index.js
+++ To do App/Backend/index.js
@@ -1,14 +1,36 @@
-const express =require("express")
+const express = require("express");
+const { createTodo, updateTodo } = require("./types");
 const app = express()
 app.use(express.json())
 
 // creating routes
 // creating a new todo
-app.post('/todo',(req,res)=>{})
+app.post('/todo', (req, res) => {
+    const createPayload = req.body;
+    const parsedPayload = createTodo.safeParse(createPayload);
+    if (!parsedPayload.success) {
+        res.status(411).json({
+            msg: "you have sent a wrong inputs"
+        })
+        return;
+    }
+    // put it in mongo db
+})
 
 
-app.get("/todos",(req,res)=>{
+app.get("/todos", (req, res) => {
 
 })
 // marking a todo completed
-app.put("/completed",(req,res)=>{})
+app.put("/completed", (req, res) => {
+    const updatePaylod = req.body;
+    const parsedPayload = updateTodo.safeParse(updatePaylod);
+    if (!parsedPayload.success) {
+
+        res.status(411).json({
+            msg: "you have sent a wrong inputs"
+        })
+        return;
+
+    }
+})
