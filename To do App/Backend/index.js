ed0b4b6d5442d26325f6cb98b872a1111ed3c4b1
Mohit Tiwari <mt0988145@gmail.com>
2026-02-12 15:35:22 +0530

    updated the databse

--- To do App/Backend/index.js
+++ To do App/Backend/index.js
@@ -1,11 +1,13 @@
 const express = require("express");
 const { createTodo, updateTodo } = require("./types");
+const {todo} = require("./db");
+const { describe } = require("zod/v4/core");
 const app = express()
 app.use(express.json())
 
 // creating routes
 // creating a new todo
-app.post('/todo', (req, res) => {
+app.post('/todo', async (req, res) => {
     const createPayload = req.body;
     const parsedPayload = createTodo.safeParse(createPayload);
     if (!parsedPayload.success) {
@@ -15,14 +17,30 @@ app.post('/todo', (req, res) => {
         return;
     }
     // put it in mongo db
+// awaiting it as db maybe Down
+await todo.create({
+    title:createPayload.title,
+    description:createPayload.description,
+    completed:false
+})
+res.json({
+    msg:"todo added sucessfully"
+})
+
 })
 
 
-app.get("/todos", (req, res) => {
-
+app.get("/todos",async (req, res) => {
+// now getting data from the databse
+const todos = await todo.find( );
+// if we do 
+//like this const todos =todo.find() and the print it It will return a promise
+res.json({
+    todos
+})
 })
 // marking a todo completed
-app.put("/completed", (req, res) => {
+app.put("/completed", async (req, res) => {
     const updatePaylod = req.body;
     const parsedPayload = updateTodo.safeParse(updatePaylod);
     if (!parsedPayload.success) {
@@ -31,6 +49,19 @@ app.put("/completed", (req, res) => {
             msg: "you have sent a wrong inputs"
         })
         return;
+        // updating the todo
 
+          await todo.update(
+            {
+                // why _id any entry in the table uniquely identified by_
+                _id:req.body.id
+            },
+            {
+                completed:true
+            }
+          )
+          res.json({
+            msg:"todo marked as completed"
+          })
     }
 })
