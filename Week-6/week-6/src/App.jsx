0d6e5d7445a149da57c21becce6786b231b5491d
Mohit Tiwari <mt0988145@gmail.com>
2026-02-17 23:33:49 +0530

    adding files

--- Week-6/week-6/src/App.jsx
+++ Week-6/week-6/src/App.jsx
@@ -1,14 +1,32 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
+import axios from "axios"
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-
+  const [todos, setTodos] = useState(0)
+useEffect(()=>{
+axios.get("http://sum-server.100xdevs.com/todos").then(res=>{
+ setTodos(res.data.todos)
+})
+},[])
   return (
-    <></>
+    <>
+  {  todos.map((todo)=>{
+        <Todos title={todo.title} description={todo.description}/>
+    })}
+    </>
   )
+}
+function Todos({title,description})
+{
+  return (<>
+  <h1>{title}</h1>
+  <p>{description}</p>
+
+  </>)
 }
 
 export default App
+// wroking with axios
