507f5114cbb2a7f252e608f13d060188cd15f6d7
Mohit Tiwari <mt0988145@gmail.com>
2026-02-18 23:06:03 +0530

    updated the effects

--- Week-6/week-6/src/App.jsx
+++ Week-6/week-6/src/App.jsx
@@ -6,6 +6,7 @@
 
   function App() {
     const [todos, setTodos] = useState([])
+    const [id,setId]=useState(1);
   useEffect(()=>{
   axios.get("http://sum-server.100xdevs.com/todos").then(res=>{
   setTodos(res.data.todos)
@@ -13,17 +14,37 @@
   },[])
     return (
       <>
-    {  todos.map((todo)=>
+      {/* this will search for all the todos */}
+    {/* {  todos.map((todo)=>
           <Todo title={todo.title} description={todo.description} key={todo+1}/>
-      )}
+      )} */}
+      {/* now searching for the specific todo using query params
+       */}
+       <button onClick={()=>setId(1)}>1</button>
+       <button onClick={()=>setId(2)}>2</button>
+       <button onClick={()=>setId(3)}>3</button>
+       <button onClick={()=>setId(4)}>4</button>
+       <Todo id={id}/>
       </>
     )
   }
-  function Todo({title,description})
+  function Todo({id})
   {
+    // will have a state for a particular todo
+      const [todo,settodo] = useState(-1);
+      // using sideffect for searching for the todo
+      useEffect(()=>{
+          axios.get(`http://sum-server.100xdevs.com/todo?id=${id}`).then(res=>settodo(res.data.todo)).catch(()=>console.log("error while fetching the id"));
+      },[id ])
     return (<>
-    <h1>{title}</h1>
-    <p>{description}</p>
+      <h1>
+        {todo.title}
+      </h1>
+      <p>
+        
+          {todo.description}
+        
+      </p>
 
     </>)
   }
