aba4c8737a34932c06942d2ec1ea0afbd3f0f245
Mohit Tiwari <mt0988145@gmail.com>
2026-02-18 22:02:29 +0530

    adding initial Files

--- Week-6/week-6/src/App.jsx
+++ Week-6/week-6/src/App.jsx
@@ -1,32 +1,32 @@
-import { useEffect, useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import axios from "axios"
-import './App.css'
+  import { useEffect, useState } from 'react'
+  import reactLogo from './assets/react.svg'
+  import viteLogo from '/vite.svg'
+  import axios from "axios"
+  import './App.css'
 
-function App() {
-  const [todos, setTodos] = useState(0)
-useEffect(()=>{
-axios.get("http://sum-server.100xdevs.com/todos").then(res=>{
- setTodos(res.data.todos)
-})
-},[])
-  return (
-    <>
-  {  todos.map((todo)=>{
-        <Todos title={todo.title} description={todo.description}/>
-    })}
-    </>
-  )
-}
-function Todos({title,description})
-{
-  return (<>
-  <h1>{title}</h1>
-  <p>{description}</p>
+  function App() {
+    const [todos, setTodos] = useState([])
+  useEffect(()=>{
+  axios.get("http://sum-server.100xdevs.com/todos").then(res=>{
+  setTodos(res.data.todos)
+  }).catch(err=>console.log("data fetching failed"))
+  },[])
+    return (
+      <>
+    {  todos.map((todo)=>
+          <Todo title={todo.title} description={todo.description} key={todo+1}/>
+      )}
+      </>
+    )
+  }
+  function Todo({title,description})
+  {
+    return (<>
+    <h1>{title}</h1>
+    <p>{description}</p>
 
-  </>)
-}
+    </>)
+  }
 
-export default App
-// wroking with axios
+  export default App
+  // wroking with axios
